5c74e6a4203bab6355786945cf680292abfd0ae5
a-velazquez <a.m.velazquez@wustl.edu>
2022-08-30 17:48:41 -0500

    update .gitignore and preliminary treaty scraping

--- bit_scraping_eda.py
+++ bit_scraping_eda.py
@@ -18,12 +18,10 @@ import requests
 from bs4 import BeautifulSoup 
 from io import BytesIO
 import PyPDF2
-import base64
+import pandas as pd
 
 url = "https://investmentpolicy.unctad.org/international-investment-agreements/iia-mapping"
 key = "treaty-files/"
-
-# ## Heading 1
 
 soup = BeautifulSoup(requests.get(url).content, "html.parser")
 
@@ -55,9 +53,6 @@ def get_bit_txt(link):
         pass
 
 
-
-all_bits2 = [get_bit_txt(l) for l in all_links]
-
 all_bits = [get_bit_txt(l) for l in all_links]
 
 len(all_links)
@@ -68,8 +63,6 @@ len(bad_links)
 
 bad_links
 
-all_bits[1:100]
-
 # +
 pdf_bytes = requests.get(all_links[50]).content
 
@@ -77,56 +70,3 @@ p = BytesIO(pdf_bytes)
 p.seek(0, os.SEEK_END)
 dir(pdf_bytes)
 pdf_bytes
-
-# +
-pdf_bytes = requests.get('https://investmentpolicy.unctad.org/international-investment-agreements/treaty-files/138/download').content
-
-p = BytesIO(pdf_bytes)
-p.seek(0, os.SEEK_END)
-dir(pdf_bytes)
-pdf_bytes
-
-# +
-import slate3k as slate
-
-with open(pdf_bytes) as f:
-    extracted_text = slate.PDF(f)
-print(extracted_text)
-# -
-
-read_pdf = PyPDF2.PdfFileReader(p)
-
-slate.PDF(read_pdf)
-
-count = read_pdf.numPages
-
-count
-
-t_page = read_pdf.getPage(3)
-
-print(t_page.extractText())
-
-import textract
-text = textract.process(pdf_bytes)
-
-first_page = read_pdf.getPage(0)
-
-dir(first_page)
-
-print(first_page)
-
-first_page.getObject()['/Parent']['/Kids'][0]
-
-encoded = first_page.getContents().getData()
-
-data = base64.b64decode(encoded)
-
-pages_txt = []
-for page_number in range(count):
-    print(page_number)
-    page = read_pdf.getPage(page_number)
-    print(page_number)
-    page_content = page.extractText()
-    pages_txt.append(page_content)
-
-pages_txt
